ca1cd671d9a194f555e1880e7e900d10e723518b
skiya <72136709+skiya@users.noreply.github.com>
2021-11-22 23:24:46 +0900

    add progress stat animation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
-let progressNumber = new Vue({
-    el: '#progress',
-    data: {
-        progress: 0
+const Progress = {
+    data() {
+        return {
+            progress: 0,
+            now: Date.now(),
+            loadingTime: 300000
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            let current = Date.now()
+            if (this.progress < 99) {
+                let progress = Math.ceil(Math.ceil(((current - this.now) / this.loadingTime) * 1000000) / 1000)
+                this.progress = progress > 100 ? 99 : progress
+                console.log(this.progress)
+            }
+        }, 1000)
+        setTimeout(() => {
+            this.progress = 100
+        }, 600000);
     }
-})
+}
 
+Vue.createApp(Progress).mount('#progress');
